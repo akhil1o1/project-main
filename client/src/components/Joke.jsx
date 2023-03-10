2af0df9cdf8e431f7bbc020bea0b84ee852c31c2
akhil panwar <akhilpanwar180@gmail.com>
2023-03-10 11:49:06 +0530

    added navbar

---
+++ client/src/components/Joke.jsx
@@ -0,0 +1,9 @@
+import { Box, Typography, Stack } from "@mui/material";
+
+function Joke() {
+    return <Box>
+        <Typography>joke test kjhdfkjsa kshdfkja kjhskdjfasd khksjdf.</Typography>
+    </Box>
+}
+
+export default Joke;
---
+++ client/src/components/Jokelist.jsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+function JokeList() {
+    return <>
+        <Typography variant="h1" fontSize="3rem" my="3rem" textAlign="center">List of Jokes</Typography>
+    </>
+}
+
+export default JokeList;
